6fa8c1650640bfd63dcfcc0731e49ef6a6759a63
nahueluriel <72473131+nahueluriel@users.noreply.github.com>
2021-11-18 11:47:44 -0300

    Update 1_ez.c

--- 1_ez.c
+++ 1_ez.c
@@ -1,3 +1,11 @@
 // escribir un programa que diga "Hola, x" donde x
 // es un argumento (string)
 // usar la funcion saludar de libprueba.h
+#include <stdio.h>
+#include <stdlib.h>
+#include "libprueba.h"
+
+int main(int argc, char *argv[]) {
+  saludar(argv[1]);
+  return;
+}
